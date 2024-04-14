70dba14a95de352c230d562a278dfe3de314b788
parisagathe <parisagathepro@gmail.com>
2024-04-14 15:46:52 -0300

    Adding the js logic

---
+++ sidebar_exercise/index.js
@@ -0,0 +1,10 @@
+const sidebar = document.getElementById("sidebar");
+const content = document.querySelector(".content");
+
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
