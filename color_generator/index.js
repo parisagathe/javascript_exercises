4154003db03c9cb5c1bc02e573f17e0d0da43504
parisagathe <parisagathepro@gmail.com>
2024-04-14 15:54:27 -0300

    Adding the js logic

---
+++ color_generator/index.js
@@ -0,0 +1,10 @@
+function getColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const color = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.background = color;
+    document.body.innerHTML = `<h2>${color}</h2>`;
+}
+setInterval(getColor, 1500);
