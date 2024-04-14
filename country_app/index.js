0a14984320705450b9cfed23385f51ad86c7cd8a
parisagathe <parisagathepro@gmail.com>
2024-04-14 17:35:20 -0300

    Taking off an unnecessary console.log()

--- country_app/index.js
+++ country_app/index.js
@@ -7,7 +7,6 @@ async function fetchCountries() {
     await fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
     .then((data) => (countriesData = data));
-    console.log(countriesData);
     countriesDisplay();
 }
 
